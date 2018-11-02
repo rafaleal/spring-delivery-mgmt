4a936f6ccaad2912c8910c01ae4ecf83a2d746c4
Rafael Leal <leal.rafa22@gmail.com>
2018-11-02 18:40:22 -0300

    Create endpoint constants

---
+++ src/main/java/app/util/EndpointConstants.java
@@ -0,0 +1,28 @@
+package app.util;
+
+/**
+ * Created by Rafael Leal on 22/10/2018.
+ */
+public class EndpointConstants {
+    public static final String API_V1 = "/v1";
+
+    //Biker endpoint
+    public static final String API_LIST_ALL_BIKERS = "/bikers";
+    public static final String API_BIKER_ID = "/biker/{id}";
+    public static final String API_NEW_BIKER = "/biker/new";
+
+    //Legal Customer endpoint
+    public static final String API_LIST_ALL_LEGAL_CUSTOMERS = "/legal-customers";
+    public static final String API_LEGAL_CUSTOMRE_ID = "/legal-customer/{id}";
+    public static final String API_NEW_LEGAL_CUSTOMER = "/legal-customer/new";
+
+    //Natural Customer endpoint
+    public static final String API_LIST_ALL_NATURAL_CUSTOMERS = "/natural-customers";
+    public static final String API_NATURAL_CUSTOMER_ID = "/natural-customer/{id}";
+    public static final String API_NEW_NATURAL_CUSTOMER = "/natural-customer/new";
+
+    //Delivery endpoint
+    public static final String API_LIST_ALL_DELIVERIES = "/deliveries";
+    public static final String API_DELIVERY_ID = "/delivery/{id}";
+    public static final String API_NEW_DELIVERY = "/delivery/new";
+}
