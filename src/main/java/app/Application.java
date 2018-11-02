ca4852074ab6619fb702d19fae138ccfaf58af03
Rafael Leal <leal.rafa22@gmail.com>
2018-11-02 18:36:42 -0300

    Extend Application from SpringBootServletInitializer

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,13 +3,14 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by Rafael Leal on 11/10/2018.
  */
 @SpringBootApplication
 @EnableConfigurationProperties
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
