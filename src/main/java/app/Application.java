5eb42b0c9948cab6c7d9106c9d6e7acaa5bd3f34
Rafael Leal <leal.rafa22@gmail.com>
2018-11-12 18:51:29 -0200

    Add ModelMapper bean

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,9 +1,11 @@
 package app;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by Rafael Leal on 11/10/2018.
@@ -13,5 +15,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
