1f0c2946d53902d3f72e99150e54d32cf05370b6
Rafael Leal <leal.rafa22@gmail.com>
2018-11-12 00:10:57 -0200

    Rename BikerRepository method name and its query

--- src/main/java/app/biker/BikerRepository.java
+++ src/main/java/app/biker/BikerRepository.java
@@ -1,9 +1,9 @@
 package app.biker;
 
+import app.biker.dto.BikerSummaryDTO;
 import app.enums.StatusCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,17 +16,23 @@ public interface BikerRepository extends JpaRepository<Biker, Long> {
 
     List<Biker> findByStatusCode(StatusCode statusCode);
 
-    @Query("SELECT new app.biker.BikerSummaryDTO(" +
+    @Query("SELECT new app.biker.dto.BikerSummaryDTO(" +
             "b.id, " +
             "b.fullName, " +
-            "COUNT(d)," +
-            "SUM(r.totalDistance)," +
+            "b.cpf, " +
+            "b.address," +
+            "b.phone," +
+            "b.email, " +
+            "b.createdAt, " +
+            "COUNT(d), " +
+            "SUM(r.totalDistance), " +
             "SUM(r.totalDue)" +
             ")" +
             " FROM Delivery AS d" +
-            " JOIN d.route r" +
-            " JOIN d.biker b" +
-            " WHERE b.id = :bikerId")
-    BikerSummaryDTO getBikerSummary(@Param("bikerId") Long bikerId);
+            " LEFT JOIN d.route r" +
+            " LEFT JOIN d.biker b" +
+            " WHERE b.statusCode = 'A'" +
+            " GROUP BY b.id")
+    List<BikerSummaryDTO> getAllActiveBikersSummary();
 
 }
